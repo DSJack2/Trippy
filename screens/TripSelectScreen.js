38ccc00953b14b3f24dcaee40e6e0adf1610a7dd
ajay1128 <32371218+ajay1128@users.noreply.github.com>
2019-02-17 20:14:57 -0600

    removed duplicated code

--- screens/TripSelectScreen.js
+++ screens/TripSelectScreen.js
@@ -1,14 +1,8 @@
 import React from 'react';
-
-import {ImageBackground, StyleSheet, View} from 'react-native';
-import {TextInput} from 'react-native-gesture-handler';
-import {Button} from 'react-native-elements';
-import {GAPIKEY} from "../constants/ApiKeys"
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View, Button } from 'react-native';
 import { StackActions, NavigationActions } from 'react-navigation';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import Geocoder from 'react-native-geocoding';
-import * as constants from '../constants/ApiKeys'
+import {GAPIKEY} from "../constants/ApiKeys"
 
 export default class TripSelectScreen extends React.Component {
     constructor(props) {
