b734d098b07bbc260cdc01c08ebe5188f39d2305
ajay1128 <32371218+ajay1128@users.noreply.github.com>
2019-02-17 19:17:47 -0600

    origin and destination markers show

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,53 +1,54 @@
 import React from 'react';
-import {StyleSheet, View,Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import MapView from 'react-native-maps';
 import {Marker} from 'react-native-maps';
 import {TextInput} from 'react-native-gesture-handler';
 import Geocoder from 'react-native-geocoding';
+import * as constants from '../constants/ApiKeys'
 
 export default class MapScreen extends React.Component {
     static navigationOptions = {
         title: 'Map',
     };
 
-    render() {
-        var startLat = '';
-        var startLng = '';
-        var endLat = '';
-        var endLng = '';
+    constructor(props) {
+        super(props);
+        this.state = {
+            startLat: 0,
+            startLng: 0,
+            endLat: 0,
+            endLng: 0,
+
+        }
+    }
+
+
+   setMarkers() {
         const {navigation} = this.props;
         const origin = navigation.getParam('origin', 'Not a Valid Address');
         const destination = navigation.getParam('destination', 'Not a Valid Address');
-        Geocoder.init(''); //need to load in api key
+        Geocoder.init(constants.GAPIKEY);
         Geocoder.from(JSON.stringify(origin))
             .then(json => {
-                 startLat = json.results[0].geometry.location.lat;
-                 startLng = json.results[0].geometry.location.lng;
+                this.setState({'startLat': json.results[0].geometry.location.lat}) ;
+                this.setState({'startLng': json.results[0].geometry.location.lng}) ;
             })
             .catch(error => console.warn(error));
         Geocoder.from(JSON.stringify(destination))
             .then(json => {
-                endLat = json.results[0].geometry.location.lat;
-                endLng = json.results[0].geometry.location.lng;
+                this.setState({'endLat': json.results[0].geometry.location.lat}) ;
+                this.setState({'endLng': json.results[0].geometry.location.lng}) ;
             })
             .catch(error => console.warn(error));
+    }
 
-        var markers = [
-            {
-                latitude: startLat,
-                longitude: startLng,
-                title: 'Origin',
-            },
-            {
-                latitude: endLat,
-                longitude: endLng,
-                title: 'Destination',
-            },
-        ];
 
+
+    render() {
+        this.setMarkers();
         return (
             <View style={styles.container}>
-                <Text>origin: {JSON.stringify(origin)}</Text>
+                {/*//<Text>origin: {JSON.stringify(origin)}</Text>*/}
                 <MapView
                     style={{flex: 1}}
                     provider="google"
@@ -56,11 +57,23 @@ export default class MapScreen extends React.Component {
                         longitude: -86.767960,
                         latitudeDelta: 0.0922,
                         longitudeDelta: 0.0421,
-                    }}
-                    annotations={markers}
-
-                />
-                <Text>destination: {JSON.stringify(destination)}</Text>
+                    }}>
+                    <MapView.Marker
+                        coordinate={{
+                            latitude: this.state.startLat,
+                            longitude: this.state.startLng,
+                        }}
+                        title={"Origin"}
+                    />
+                    <MapView.Marker
+                        coordinate={{
+                            latitude: this.state.endLat,
+                            longitude: this.state.endLng
+                        }}
+                        title={"Destination"}
+                    />
+                </MapView>
+                {/*<Text>destination: {JSON.stringify(destination)}</Text>*/}
             </View>
         );
     }
@@ -70,4 +83,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+
 });
+
